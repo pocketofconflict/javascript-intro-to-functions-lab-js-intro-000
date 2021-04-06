aa7929be440cc4944325b0fdd892c197f23f394f
Christopher Diaz <pocketofconflict@gmail.com>
2021-04-06 18:43:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if string == string.toLowerCase()
-  return string.toLowerCase
+  return string.toLowerCase()
   if string == string.toUpperCase()
   return string.toUpperCase()
 }
